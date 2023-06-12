ec2740363d0a6e0e3d2a79ac030236865e533fc4
Alex Tetervak <tetervak@sheridancollege.ca>
2023-06-12 05:43:07 -0400

    added more list options

--- src/main/java/ca/tetervak/courselistdemo/controller/CourseDataController.java
+++ src/main/java/ca/tetervak/courselistdemo/controller/CourseDataController.java
@@ -7,9 +7,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class CourseDataController {
@@ -22,11 +25,35 @@ public class CourseDataController {
         this.repository = repository;
     }
 
-    @GetMapping(value = {"/", "/courses"})
-    ModelAndView listCourses(@RequestParam(defaultValue = "0") int p){
+    @GetMapping(value = {"/", "/index"})
+    public String index(){
+        return "Index";
+    }
+
+    @GetMapping("/list-all")
+    public ModelAndView listAllCourses(){
+        List<Course> courses = repository.findAll();
+        return new ModelAndView("ListAllCourses", "courses", courses);
+    }
+
+    @GetMapping("/list-by-pages")
+    public ModelAndView listCoursesByPages(@RequestParam(defaultValue = "0") int p){
 
         Page<Course> page = repository.findAll(PageRequest.of(p,10));
-        return new ModelAndView("Courses", "page", page);
+        return new ModelAndView("ListCoursesByPages", "page", page);
+    }
+
+    @GetMapping("/list-by-codes")
+    public String listCoursesByCodes(
+            @RequestParam(defaultValue = "PROG") String starts,
+            Model model
+    ){
+        String[] codeLetters = {"APPL", "COMM", "DBAS", "DSGN", "INFO", "MATH", "MEDA", "PROG", "SYST", "TELE"};
+        List<Course> courses = repository.findByCodeStartsWith(starts);
+        model.addAttribute("codeLetters", codeLetters);
+        model.addAttribute("selectedLetters", starts);
+        model.addAttribute("courses", courses);
+        return"ListCoursesByCodes";
     }
 
 }
--- src/main/java/ca/tetervak/courselistdemo/data/CourseRepository.java
+++ src/main/java/ca/tetervak/courselistdemo/data/CourseRepository.java
@@ -2,5 +2,9 @@ package ca.tetervak.courselistdemo.data;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CourseRepository extends JpaRepository<Course, Integer> {
+
+    List<Course> findByCodeStartsWith(String starts);
 }
